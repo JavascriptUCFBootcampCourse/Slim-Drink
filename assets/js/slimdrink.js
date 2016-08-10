29c53103ae06211c4476bb20a7c8d4632169423d
CodingBootCamp <codingbootcamp@Jorges-MacBook-Pro.local>
2016-08-09 20:07:29 -0400

    storing everything in local storage

--- assets/js/slimdrink.js
+++ assets/js/slimdrink.js
@@ -9,30 +9,32 @@ $(document).ready(function()
 	var usdaKey = '8XH1PqPkLb7GfZa9BMvfIqM8cpnjaBIzMCQ1pfkB';
 	var usdaSearchQueryUrl = 'http://api.nal.usda.gov/ndb/search/?format=json&q=%ALCOHOL%&api_key=' + usdaKey;
 	var usdaFoodReportQueryUrl = 'http://api.nal.usda.gov/ndb/reports/?ndbno=%NDBNO%&type=f&format=json&api_key=' + usdaKey;
-	var slimDrink = JSON.parse(localStorage.getItem('slimDrink'));
+	var slimDrink = JSON.parse(localStorage.getItem('slimDrink')) || {};
 	var currentWeight, currentCalories, gender;
+	var test = [];
 
 
-	if(slimDrink != null)
+	if(slimDrink != undefined)
 	{
 		currentWeight = slimDrink.currentWeight;
-		currentCalories = slimDrink.currentCalories;
+		maxCalories = slimDrink.maxCalories;
 		gender = slimDrink.gender;
 		alcoholChoice = slimDrink.alcoholChoice;
 	}
 
-	$('#step1form').submit(function()
+	$('#step1form').submit(function(e)
 	{
+		e.preventDefault();
 		weight = $('#weight').val().trim();
 		maxCalories = $('#calories').val().trim();
 		console.log('weight = ' + weight);
 		console.log('calories = ' + maxCalories);
 
-		localStorage.setItem('slimDrink', JSON.stringify(
-			{
-				currentWeight: weight,
-				currentCalories: maxCalories
-			}));
+		slimDrink.currentWeight = weight;
+		slimDrink.maxCalories = maxCalories;
+		slimDrink.gender = 'gender';
+
+		localStorage.setItem('slimDrink', JSON.stringify(slimDrink));
 
 		return false;
 	});
@@ -40,7 +42,6 @@ $(document).ready(function()
 	$('.alcohol').click(function()
 	{
 		alcoholChoice = $(this).attr('id');
-		console.log('alcoholChoice = '+alcoholChoice);
 
 		$.ajax({url: usdaSearchQueryUrl.replace('%ALCOHOL%', alcoholChoice), method:'GET'})
 		.done(function(response)
@@ -53,12 +54,24 @@ $(document).ready(function()
 				var nutrients = response2.report.food.nutrients;
 				for(var i = 0; i < nutrients.length; i++)
 				{
-					if(nutrients[i].name === 'Alcohol, ethyl' || (nutrients[i].name === 'Energy' && nutrients[i].unit === 'kcal'))
-						console.log(nutrients[i]);
+					if(nutrients[i].name === 'Alcohol, ethyl')
+						slimDrink.alcoholContent = nutrients[i].value;
+					else if(nutrients[i].name === 'Energy' && nutrients[i].unit === 'kcal')
+						slimDrink.maxCalories = nutrients[i].value;
 				}
+				localStorage.setItem('slimDrink', JSON.stringify(slimDrink));
 			});
 		});
 	});
 
+	function reset()
+	{
+		currentWeight = 0;
+		maxCalories = 0;
+		gender = '';
+		alcoholChoice = 0;
+		calories = 0;
+		maxCalories = 0;
 
+	}
 })
