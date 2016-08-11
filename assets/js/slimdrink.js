ee6e6206b6c1259bd53baea798d483f71b4c2ce7
CodingBootCamp <codingbootcamp@Jorges-MacBook-Pro.local>
2016-08-10 21:39:30 -0400

    updating javascript implementation

--- assets/js/slimdrink.js
+++ assets/js/slimdrink.js
@@ -1,37 +1,23 @@
 $(document).ready(function()
 {
+	/*
+		TODO: 
+		- capture gender
+		- potentially fix bac formula
+		- create divs for each hours alcohol consumption
+	*/
 	var nutritionixAppId = '66148b0c';
 	var nutritionixAppKey = 'fa8f5f681a9b4939d83631e3230ebb6d';
-	var queryUrl = 'https://api.nutritionix.com/v1_1/search/%ALCOHOL%?fields=*&appId='+nutritionixAppId+'&appKey='+nutritionixAppKey;
-	var weight = 140;
-	var maxCalories = 600;
-	var alcoholChoice = 'wine';
+	var nutritionixQueryUrl = 'https://api.nutritionix.com/v1_1/search/%ALCOHOL%?fields=*&appId='+nutritionixAppId+'&appKey='+nutritionixAppKey;
 	var usdaKey = '8XH1PqPkLb7GfZa9BMvfIqM8cpnjaBIzMCQ1pfkB';
-	var usdaSearchQueryUrl = 'http://api.nal.usda.gov/ndb/search/?format=json&q=%ALCOHOL%&api_key=' + usdaKey;
+	var usdaSearchQueryUrl = 'http://api.nal.usda.gov/ndb/search/?format=json&fg=1400&q=%ALCOHOL%&api_key=' + usdaKey;
 	var usdaFoodReportQueryUrl = 'http://api.nal.usda.gov/ndb/reports/?ndbno=%NDBNO%&type=f&format=json&api_key=' + usdaKey;
 	var slimDrink = JSON.parse(localStorage.getItem('slimDrink')) || {};
-	var currentWeight, currentCalories, gender;
-	var test = [];
 
-
-	if(slimDrink != undefined)
+	$('#step1form').submit(function()
 	{
-		currentWeight = slimDrink.currentWeight;
-		maxCalories = slimDrink.maxCalories;
-		gender = slimDrink.gender;
-		alcoholChoice = slimDrink.alcoholChoice;
-	}
-
-	$('#step1form').submit(function(e)
-	{
-		e.preventDefault();
-		weight = $('#weight').val().trim();
-		maxCalories = $('#calories').val().trim();
-		console.log('weight = ' + weight);
-		console.log('calories = ' + maxCalories);
-
-		slimDrink.currentWeight = weight;
-		slimDrink.maxCalories = maxCalories;
+		slimDrink.currentWeight = $('#weight').val().trim();
+		slimDrink.targetCalories = $('#calories').val().trim();
 		slimDrink.gender = 'gender';
 
 		localStorage.setItem('slimDrink', JSON.stringify(slimDrink));
@@ -41,37 +27,63 @@ $(document).ready(function()
 
 	$('.alcohol').click(function()
 	{
-		alcoholChoice = $(this).attr('id');
-
+		var alcoholChoice = $(this).attr('id');
+		slimDrink.alcoholChoice = alcoholChoice;
+		alcoholChoice = alcoholChoice === 'beer' ? 'regular beer' : alcoholChoice;
+		
 		$.ajax({url: usdaSearchQueryUrl.replace('%ALCOHOL%', alcoholChoice), method:'GET'})
 		.done(function(response)
 		{
-			console.log(response);
-			var ndbno = response.list.item[1].ndbno;
+			console.log(response.list.item);
+			var ndbno = response.list.item[0].ndbno;
 			$.ajax({url: usdaFoodReportQueryUrl.replace('%NDBNO%', ndbno), method:'GET'})
 			.done(function(response2)
 			{
+				console.log(response2);
 				var nutrients = response2.report.food.nutrients;
+
 				for(var i = 0; i < nutrients.length; i++)
 				{
 					if(nutrients[i].name === 'Alcohol, ethyl')
-						slimDrink.alcoholContent = nutrients[i].value;
+						slimDrink.alcoholContent = nutrients[i].measures[1].value;
 					else if(nutrients[i].name === 'Energy' && nutrients[i].unit === 'kcal')
-						slimDrink.maxCalories = nutrients[i].value;
+						slimDrink.alcoholCalories = nutrients[i].measures[1].value;
 				}
 				localStorage.setItem('slimDrink', JSON.stringify(slimDrink));
 			});
 		});
 	});
 
-	function reset()
+	function calculateBAC(hours)
 	{
-		currentWeight = 0;
-		maxCalories = 0;
-		gender = '';
-		alcoholChoice = 0;
-		calories = 0;
-		maxCalories = 0;
+		var standardDrink;
+		var genderVar;
 
+		switch(slimDrink.alcoholChoice)
+		{
+			case 'beer':
+				standardDrink = 12;
+				break;
+			case 'wine':
+				standardDrink = 5;
+				break;
+			case 'liquor':
+				standardDrink = 2;
+				break;
+		}
+
+		standardDrink *= calculateNumberOfDrinks();
+
+		if(slimDrink.gender === 'male')
+			genderVar = .68;
+		else
+			genderVar = .55;
+
+		return (standardDrink * slimDrink.alcoholContent)/(slimDrink.currentWeight * 454 * genderVar) * 100 * hours;
+	}
+
+	function calculateNumberOfDrinks()
+	{
+		return Math.floor(slimDrink.targetCalories / slimDrink.alcoholCalories);
 	}
 })
